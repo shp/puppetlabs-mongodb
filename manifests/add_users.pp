54fbff5e54c7f99f5b5fdec0a18f547595937ad5
Dimitar Roustchev <dimitar.roustchev@stylight.com>
2013-02-20 18:12:46 +0100

    Added support for creating users in MongoDB via the mongo shell.

---
+++ manifests/add_users.pp
@@ -0,0 +1,30 @@
+define mongodb::add_users(
+  $db
+  $users
+)
+{
+  if defined($db){
+    if defined($users){
+  
+      include mongodb
+  
+      file {"/tmp/add_users.js":
+        content => template('mongodb/add_users.js.erb'),
+        owner => mongodb,
+        group => mongodb,
+        mode => 644,
+        require => Service['mongodb']
+      }
+      
+      exec {"add_users":
+        command => "mongo ${db} /tmp/add_users.js",
+        
+      }
+    
+    }
+    if !defined($users){fail("No users specified to be added the mongodb: ${db}")}
+  }
+  if !defined($db){fail("No db specified for adding users.")} 
+}
+
+
