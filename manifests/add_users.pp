7bc630804e4855d71ec1487634166d2033123197
Dimitar Roustchev <dimitar.roustchev@stylight.com>
2013-02-25 10:04:21 +0100

    Fixed empty users array check and indents.

--- manifests/add_users.pp
+++ manifests/add_users.pp
@@ -8,21 +8,23 @@ define mongodb::add_users(
   
       include mongodb
   
-      file {"/tmp/add_users.js":
+      file {"add_users.js":
+        path => '/tmp/add_users.js',
+        ensure  => file,
         content => template('mongodb/add_users.js.erb'),
-        owner => mongodb,
-        group => mongodb,
-        mode => 644,
+        owner   => mongodb,
+        group   => mongodb,
+        mode    => 644,
         require => Service['mongodb']
       }
       
       exec {"add_users":
         command => "mongo ${db} /tmp/add_users.js",
-        
+        require => File['add_users.js']
       }
     
     }
-    if !defined($users){fail("No users specified to be added the mongodb: ${db}")}
+    if empty($users){fail("No users specified to be added to ${db}")}
   }
   if !defined($db){fail("No db specified for adding users.")} 
 }
