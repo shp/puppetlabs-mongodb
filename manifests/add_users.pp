98d928d9cfcfb811319f1a6fe90b1293db543997
Dimitar Roustchev <dimitar.roustchev@stylight.com>
2013-02-25 23:29:14 +0100

    Fixed resource type for adding users to Mongodb.

--- manifests/add_users.pp
+++ manifests/add_users.pp
@@ -1,30 +1,29 @@
 define mongodb::add_users(
-  $db,
+  $db = $title,
   $users
 )
 {
-  if defined($db){
-    if !empty($users){
-  
-      include mongodb
-  
-      file {"add_users.js":
-        path    => '/tmp/add_users.js',
-        ensure  => file,
-        content => template('mongodb/add_users.js.erb'),
-        owner   => mongodb,
-        group   => mongodb,
-        mode    => 644,
-        require => Service['mongodb']
-      }
-      
-      exec {"add_users":
-        command => "mongo ${db} /tmp/add_users.js",
-        require => File['add_users.js']
-      }
-    
+  if $db == undef {fail("No db specified for adding users.")}
+  if !empty($users){
+
+    include mongodb
+
+    $filename = "add_${db}_users.js"
+    file {"${filename}":
+      path    => "/tmp/${filename}",
+      ensure  => file,
+      content => template('mongodb/add_users.js.erb'),
+      owner   => mongodb,
+      group   => mongodb,
+      mode    => 644,
+      require => Service['mongodb']
     }
-    if empty($users){fail("No users specified to be added to ${db}")}
+    
+    exec {"add_users":
+      command => "mongo ${db} /tmp/${filename}",
+      require => File["${filename}"]
+    }
+
   }
-  if !defined($db){fail("No db specified for adding users.")} 
+  if empty($users){fail("No users specified to be added to ${db}")}
 }
